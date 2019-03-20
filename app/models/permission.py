b4bded197b2ee7ed14bdfb8cd3b4d13a8223aeb6
Ahasan Habib Sajib <ahasan@bongobd.com>
2019-03-20 12:55:24 +0600

    lint code, to solely rely on marshmallow validation & serialization

--- app/models/permission.py
+++ app/models/permission.py
@@ -4,8 +4,6 @@ import uuid
 from sqlalchemy import desc, asc, or_
 from sqlalchemy import inspect
 from sqlalchemy.dialects.postgresql import UUID
-
-
 from . import db, ma
 
 class PermissionModel(db.Model):
@@ -15,7 +13,7 @@ class PermissionModel(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = db.Column(db.String(100), nullable=False)
     code = db.Column(db.String(30), unique=True, nullable=False)
-    active = db.Column(db.Boolean,nullable=False)
+    active = db.Column(db.Boolean, nullable=False)
     created_by = db.Column(db.String(100), nullable=True)
     updated_by = db.Column(db.String(100), nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow, nullable=False)
@@ -31,42 +29,44 @@ class PermissionModel(db.Model):
         """Permission save method."""
         db.session.add(self)
         if commit is True:
-            result = db.session.commit()
-            return result     
+            db.session.commit()
+        
 
     def get_permission(self, **kwargs):
         """ this is common method for returing list of permission including filter & search"""
         search = kwargs.get('search', None)
-        sortBy  = kwargs.get('sort_by','active')
-        orderBy =  kwargs.get('order_by','asc')
-        limit = kwargs.get('limit',10)
-        offset = kwargs.get('offset',0)
-        filter = {} #{'active' : True}
-        #check whether there is filtering option for that
+        sortBy = kwargs.get('sort_by', 'active')
+        orderBy = kwargs.get('order_by', 'asc')
+        limit = kwargs.get('limit', 10)
+        offset = kwargs.get('offset', 0)
+        filter = {}  # {'active' : True}
+        # check whether there is filtering option for that
         mapper = inspect(PermissionModel)
         for column in mapper.attrs:
             if kwargs.get(column.key):
                 filter[column.key] = kwargs.get(column.key)
-        # print(filter)
+
         allPermission = PermissionModel.query
         if search:
-            result = allPermission.filter(or_(PermissionModel.name.like('%'+search+'%'),PermissionModel.code.like('%'+search+'%'))).order_by(desc(getattr(PermissionModel, sortBy))).offset(offset).limit(limit).all()
+            result = allPermission.filter(or_(PermissionModel.name.like('%'+search+'%'), PermissionModel.code.like(
+                '%'+search+'%'))).order_by(desc(getattr(PermissionModel, sortBy))).offset(offset).limit(limit).all()
         elif filter:
             result = allPermission.filter_by(**filter).offset(offset).limit(limit).all()
-        # this will sort by field name either asc or desc 
+        # this will sort by field name either asc or desc
         elif sortBy:
             if orderBy == 'desc':
-                result = allPermission.order_by(desc(getattr(PermissionModel, sortBy))).offset(offset).limit(limit).all()
+                result = allPermission.order_by(desc(getattr(PermissionModel, sortBy))
+                                                ).offset(offset).limit(limit).all()
             else:
                 result = allPermission.order_by(asc(getattr(PermissionModel, sortBy))).offset(offset).limit(limit).all()
         else:
             result = allPermission.offset(offset).limit(limit).all()
-        return result 
+        return result
 
 
 class PermissionSchema(ma.ModelSchema):
     """Permission Schema """
-    class Meta: 
+    class Meta:
         """ Meta class """
         model = PermissionModel
         fields = ("id", "name", "code", "active", 'created_at', 'updated_at')
