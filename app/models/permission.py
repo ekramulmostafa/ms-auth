d1910664edd7e4459fbbf45942c96cf388e4d464
Ahasan Habib Sajib <ahasan@bongobd.com>
2019-03-28 16:22:44 +0600

    flake

--- app/models/permission.py
+++ app/models/permission.py
@@ -35,35 +35,35 @@ class PermissionModel(db.Model):
     def get_permission(self, **kwargs):
         """ this is common method for returing list of permission including filter & search"""
         search = kwargs.get('search', None)
-        sortBy = kwargs.get('sort_by', 'active')
-        orderBy = kwargs.get('order_by', 'asc')
+        sort_by = kwargs.get('sort_by', 'active')
+        order_by = kwargs.get('order_by', 'asc')
         limit = kwargs.get('limit', 10)
         offset = kwargs.get('offset', 0)
-        filterBy = {}
+        filter_by = {}
         # check whether there is filtering option for that
         mapper = inspect(PermissionModel)
         for column in mapper.attrs:
             if kwargs.get(column.key):
-                filterBy[column.key] = kwargs.get(column.key)
+                filter_by[column.key] = kwargs.get(column.key)
 
-        allPermission = PermissionModel.query
+        all_permission = PermissionModel.query
         if search:
-            result = allPermission.filter(
+            result = all_permission.filter(
                 or_(PermissionModel.name.like('%'+search+'%'), PermissionModel.code.like(
-                    '%'+search+'%'))).order_by(desc(getattr(PermissionModel, sortBy))
+                    '%'+search+'%'))).order_by(desc(getattr(PermissionModel, sort_by))
                                                ).offset(offset).limit(limit).all()
-        elif filterBy:
-            result = allPermission.filter_by(**filterBy).offset(offset).limit(limit).all()
+        elif filter_by:
+            result = all_permission.filter_by(**filter_by).offset(offset).limit(limit).all()
         # this will sort by field name either asc or desc
-        elif sortBy:
-            if orderBy == 'desc':
-                result = allPermission.order_by(desc(getattr(PermissionModel, sortBy))
-                                                ).offset(offset).limit(limit).all()
+        elif sort_by:
+            if order_by == 'desc':
+                result = all_permission.order_by(desc(getattr(PermissionModel, sort_by))
+                                                 ).offset(offset).limit(limit).all()
             else:
-                result = allPermission.order_by(asc(getattr(PermissionModel, sortBy))
-                                                ).offset(offset).limit(limit).all()
+                result = all_permission.order_by(asc(getattr(PermissionModel, sort_by))
+                                                 ).offset(offset).limit(limit).all()
         else:
-            result = allPermission.offset(offset).limit(limit).all()
+            result = all_permission.offset(offset).limit(limit).all()
         return result
 
 
