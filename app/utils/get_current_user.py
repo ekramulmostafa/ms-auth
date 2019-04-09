078aaca68fe1971a3bbffd55a7f6529c0070382b
Ekramul Mostafa <ekramul@bongobd.com>
2019-04-09 10:08:58 +0600

    lint fix

--- app/utils/get_current_user.py
+++ app/utils/get_current_user.py
@@ -6,5 +6,6 @@ logger = Logger(__name__)
 
 
 def get_current_user(uuid='1000211211101'):
+    """ Get Current User UUID """
     logger.info("User Detail get", data={'uuid': str(uuid)})
     return uuid
