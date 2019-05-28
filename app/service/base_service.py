03a373b403781edd2d7ac97a3ea67413dcc95029
Ekramul Mostafa <ekramul@bongobd.com>
2019-05-28 12:39:49 +0600

    service done

--- app/service/base_service.py
+++ app/service/base_service.py
@@ -17,27 +17,41 @@ class BaseService:
         Model = self.Meta.__model__
         # print(Model)
         results = Model.query
-        results = self.__searchResults(results, values['query_string'])
-        results = self.__offsetLimit(results, values['limit'], values['offset'])
-        results = self.__orderByResults(results)
+        results = self.__searchResults(results, values)
         results = self.__filterByResults(results, values)
+        results = self.__orderByResults(results)
+        results = self.__offsetLimit(results, values)
         results = results.all()
 
         return results
 
-    def __offsetLimit(self, results, limit, offset):
-        """ results offset limit """
-        if limit != None and int(limit) > 0:
+    def __offsetLimit(self, results, values):
+        """ 
+            results offset limit 
+            limit and offset parameters needed for results to use limit, offset
+        """
+        limit = None
+        offset = None
+        if 'limit' in values and values['limit'] != None:
+            limit = values['limit']
+        if 'offset' in values and values['offset'] != None:
+            offset = values['offset']
+
+        if int(limit) > 0:
             results = results.limit(limit)
 
-        if limit != None and int(offset) > 0:
+        if int(offset) > 0:
             results = results.offset(offset)
         return results
 
-    def __searchResults(self, results, value):
-        """ search in searchable fields of the object """
+    def __searchResults(self, results, values):
+        """ 
+            search in searchable fields of the object 
+            query_string parameter for search in searchable field(s)
+        """
         Model = self.Meta.__model__
-        if value != None:
+        if 'query_string' in values and values['query_string'] != None:
+            value = values['query_string']
             for field in self.Meta.__searchable__:
                 if field in Model.__dict__:
                     results = results.filter(Model.__dict__[field].like("%" + value + "%"))
@@ -65,21 +79,46 @@ class BaseService:
         filterable = self.Meta.__filterable__
 
         for key, val in values.items():
-            if (val != None and val != 0 and val != "") and self.__filterableFieldChecking(key):
-                if val.find(','):
-                    pass
-                elif val.find('_'):
-                    pass
-                print(key, val)
+            if (val != None and val != 0 and val != "") and self.__filterableFieldChecking(filterable, key):
+                results = results.filter(self.__expressionReturn(Model, key, val))
 
         return results
 
-    def __filterableFieldChecking(self, key_value_pair):
+    def __filterableFieldChecking(self, filterable, key_value_pair):
         """ filterableFieldChecking """
-        print("key value pair")
-        print(key_value_pair)
-        filterable = self.Meta.__filterable__
+
         for field in filterable:
             if key_value_pair == field:
                 return True
         return False
+
+    def __expressionReturn(self, Model, field, val):
+        """
+            filter fields of the object
+        """
+        expression = None
+        if val.find(',') > 0:
+            arr = val.split(',')
+            # print(arr)
+            expression = Model.__dict__[field].between(arr[0], arr[1])
+
+        elif val.find('_') > 0:
+            arr = val.split('_')
+            operator = arr[1]
+            value = arr[0]
+            if operator == "gt" and field in Model.__dict__:
+                expression = Model.__dict__[field] > value
+
+            elif operator == "gteq" and field in Model.__dict__:
+                expression = Model.__dict__[field] >= value
+
+            elif operator == "lt" and field in Model.__dict__:
+                expression = Model.__dict__[field] < value
+
+            elif operator == "lteq" and field in Model.__dict__:
+                expression = Model.__dict__[field] <= value
+
+        else:
+            expression = Model.__dict__[field] == val
+
+        return expression
--- app/service/role_service.py
+++ app/service/role_service.py
@@ -6,6 +6,11 @@ from app.models.role import Role
 class RoleService(BaseService):
     """ Role service """
     class Meta:
+        """ 
+            __searchable__ = fields in where checking the values
+            __sortable__ = if use ! operator it will do desc operation else asc operation in the fields
+            __filterable__ = fields for filtering ( operators are: rang(,) , >(_gt), <(_lt), >=(_gteq), <=(_lteq) . But for equals no need to use any operator, just (field = value) will be sufficent )
+        """
         __model__ = Role
         __searchable__ = ['name']
         __sortable__ = ['id', '!created_at', 'updated_at']
